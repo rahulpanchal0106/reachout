f98dc7a599d6f1eadd261a8c481783ec94ac5a11
Rahul Panchal <rm5901960@gmail.com>
2024-10-12 09:42:08 +0530

    Add drop-shadow custom property

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,13 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+        'glow': [
+            '0 0px 35px var(--glow-color)',
+            // '0 45px 65px var(--glow-color)'
+        ]
+      }
     },
   },
   plugins: [],
